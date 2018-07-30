5f51c42af166a8c6870b8a2ede5e0eb87f71c1c6
lalithakasu <41769544+lalithakasu@users.noreply.github.com>
2018-07-30 10:46:38 +0530

    Create porn.java

---
+++ porn.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class porn
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+ 		if(a>0)
+		{
+			System.out.println("Positive");
+		}
+		else if(a<0)
+		{
+			System.out.println("Negative");
+        }
+		else
+		{
+			System.out.println("Zero");
+		}
+	}
+}
