eef0b2ca0791937688bf497dbd85df9365733781
lalithakasu <41769544+lalithakasu@users.noreply.github.com>
2018-08-13 12:53:40 +0530

    Create fibb.java

---
+++ fibb.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class fibb
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int t1=0,t2=1;
+		int sum=0;
+		int i;	
+		for(i=0;i<n;i++)
+		{
+			System.out.println(t2+" ");
+			sum=t1+t2;
+			t1=t2;
+			t2=sum;
+			
+				
+			
+			
+		}
+	}
+}
