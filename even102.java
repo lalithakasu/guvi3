2cd83ce40c5c3853fbe6a6e7771dd434b0ba959c
lalithakasu <41769544+lalithakasu@users.noreply.github.com>
2018-08-16 11:07:53 +0530

    Update and rename 102.java to even102.java

--- even102.java
+++ even102.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class 102
+class even102
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
@@ -13,7 +13,6 @@ class 102
 		int n;
 		Scanner sc=new Scanner(System.in);
 		n=sc.nextInt();
-		int res=0;
 		while(n%2==0)
 		{
 			n=n/2;
