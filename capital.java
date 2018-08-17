90493056ab516c1216f8754d654ca63c5b519818
lalithakasu <41769544+lalithakasu@users.noreply.github.com>
2018-08-17 09:44:42 +0530

    Create capital.java

---
+++ capital.java
@@ -0,0 +1,43 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class capital
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));   
+
+System.out.println("Enter the sentence : ");
+
+try
+{
+    String str = br.readLine();
+    char[] str1 = new char[str.length()];
+
+    for(int i=0; i<str.length(); i++)
+    {
+        str1[i] = Character.toLowerCase(str.charAt(i));
+    }
+
+    str1[0] = Character.toUpperCase(str1[0]);
+    for(int i=0;i<str.length();i++)
+    {
+        if(str1[i] == ' ')
+        {                   
+            str1[i+1] =  Character.toUpperCase(str1[i+1]);
+        }
+        System.out.print(str1[i]);
+    }
+}
+catch(Exception e)
+{
+    System.err.println("Error: " + e.getMessage());
+}
+
+	}
+}
