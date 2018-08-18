a17490efe63dea88bd0c3b5f1927565a5e510768
lalithakasu <41769544+lalithakasu@users.noreply.github.com>
2018-08-18 12:02:30 +0530

    Create noofch.java

---
+++ noofch.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class noofch
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here/* package whatever; // don't place package name! */
+
+
+	
+		int count=0,i;
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		for(i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)!=' ')
+			{
+				count++;
+			}
+		}
+			System.out.println(count);
+		
+	}
+
+
+	}
