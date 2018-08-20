a7aa74e40a6a318ec8265456a87ef9090e65a396
lalithakasu <41769544+lalithakasu@users.noreply.github.com>
2018-08-20 10:35:48 +0530

    Create armstrong.java

---
+++ armstrong.java
@@ -0,0 +1,34 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class armstrong
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		
+		for(int i=a;i<b;i++)
+		{
+			int res=0,rem=0;
+			int temp=i;
+		while(temp>0)
+		{
+			rem=temp%10;
+			res=res+rem*rem*rem;
+			temp=temp/10;
+		}
+		if(i==res)
+		{
+			System.out.println(i + "\t");
+		}
+		}
+		
+	}
+}
