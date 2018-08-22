2cf9e524d5bf5c93b447080299fdbc19317629eb
lalithakasu <41769544+lalithakasu@users.noreply.github.com>
2018-08-22 21:17:26 +0530

    Create prog41.java

---
+++ prog41.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class prog41
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n;
+		int i;
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		n=sc.nextInt();
+		for(i=0;i<n;i++)
+		{
+			System.out.println(s);
+			System.out.println();
+		}
+	}
+}
