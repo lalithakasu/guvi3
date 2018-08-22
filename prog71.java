2e6ae499a9c35b135c8239632dc6972713831857
lalithakasu <41769544+lalithakasu@users.noreply.github.com>
2018-08-22 12:38:52 +0530

    Create prog71.java

---
+++ prog71.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class prog71
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		 String a, b = "";
+        Scanner s = new Scanner(System.in);
+        a = s.nextLine();
+        int n = a.length();
+        for(int i = n - 1; i >= 0; i--)
+        {
+            b = b+a.charAt(i);
+        }
+        if(a.equalsIgnoreCase(b))
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+	}
+}
