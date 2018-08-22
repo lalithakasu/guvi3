896ed0fb973a18152042a5071aaa13c00b0eed84
lalithakasu <41769544+lalithakasu@users.noreply.github.com>
2018-08-22 11:57:25 +0530

    Create prog67.java

---
+++ prog67.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class prog67
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		a=(a/10)*10;
+		a=a+10;
+		System.out.println(a);
+	}
+}
