19f050fd79169a10c6585cb564c4e78b5b2acef8
lalithakasu <41769544+lalithakasu@users.noreply.github.com>
2018-08-22 21:41:46 +0530

    Create prog43.java

---
+++ prog43.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class prog43
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.next();
+		String s2=sc.next();
+		String res=s1+s2;
+		System.out.println(res);
+	}
+}
