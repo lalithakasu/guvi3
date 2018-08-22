8fa25ce7dcddda39215d29fc29cdde7e291db25e
lalithakasu <41769544+lalithakasu@users.noreply.github.com>
2018-08-22 15:56:16 +0530

    Create prog27.java

---
+++ prog27.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class prog27
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		boolean numeric = true;
+
+        try {
+            Double num = Double.parseDouble(str);
+        } catch (NumberFormatException e) {
+            numeric = false;
+        }
+
+        if(numeric)
+            System.out.println(" yes");
+        else
+            System.out.println("no");
+    }
+
+}
