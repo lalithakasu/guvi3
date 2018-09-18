060145b318a5b77942dd6c2fb3bb12954b471105
lalithakasu <41769544+lalithakasu@users.noreply.github.com>
2018-09-18 12:57:33 +0530

    Create prog82.java

---
+++ prog82.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class prog82
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		float n1=sc.nextFloat();
+		float n2=sc.nextFloat();
+	    double	res=n1*n2;
+	 System.out.format("%.5f", res);
+	}
+}
